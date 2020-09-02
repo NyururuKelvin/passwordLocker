9582b47c2a006500ec25f4c20d332314a6ca403f
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-02 15:51:11 +0300

    my commit declare class TestUsers & Doctring

--- locker_test.py
+++ locker_test.py
@@ -2,3 +2,11 @@ import unittest
 from locker import User
 from locker import Credentials
 import pyperclip
+
+class TestUsers(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
