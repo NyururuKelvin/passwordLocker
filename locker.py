b0d7e528a99e39cc0d84027ad8f73b1bc664a9f1
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-02 15:44:11 +0300

    my commit add empty lists & initializing method

--- locker.py
+++ locker.py
@@ -5,7 +5,11 @@ class User:
 
     """
     
-    pass
+    users_list = []
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
 
 class Credentials:
 
@@ -14,4 +18,20 @@ class Credentials:
 
     """
 
-    pass
+    credentials_list = []
+
+    def __init__(self, account, username, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+        
+        Args:
+            account: New credentials account name.
+            username : New credentials password.
+            password: New credentials password.
+           
+        '''
+
+        self.account = account
+        self.username = username
+        self.password = password
