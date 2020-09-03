26a5ed140a789329bf96c47864a2ee7a060799af
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-03 16:15:07 +0300

    my commit add methods for saving & deleting

--- locker.py
+++ locker.py
@@ -62,3 +62,19 @@ class Credentials:
         self.account = account
         self.username = username
         self.password = password
+
+    def save_credentials(self):
+
+        '''
+        save_credentials method saves credentials objects into credentials_list
+        '''
+
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+
+        '''
+        delete_credentials method deletes a saved credentials from the credentials_list
+        '''
+        Credentials.credentials_list.remove(self)
+
