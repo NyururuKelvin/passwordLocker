171eeec43259352a163c27ca9c292c5e55a2808c
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-04 17:22:56 +0300

    my commit add methods

--- run.py
+++ run.py
@@ -10,3 +10,27 @@ def create_account(user_name, password):
 
     new_user = User(user_name, password)
     return new_user
+
+def save_account(user):
+    '''
+    A function that saves a user
+    '''
+
+    User.create_account(user)
+
+def login(user_name, password):
+    '''
+    A function that checks if a user exists and returns a  boolean
+    '''
+
+    return User.user_exist(user_name, password)
+
+def create_credentials(account, username, password):
+    '''
+    A function that creates new credentials
+    '''
+    new_credentials = Credentials(account, username, password)
+    return new_credentials
+
+def save_credentials(credentials):
+    pass
