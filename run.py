2fdb520405dafb5a79180a429ffeeb6217a4d522
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-05 21:01:57 +0300

    my commit add functions

--- run.py
+++ run.py
@@ -33,4 +33,36 @@ def create_credentials(account, username, password):
     return new_credentials
 
 def save_credentials(credentials):
-    pass
+    '''
+    A function that saves credentials
+    '''
+
+    credentials.save_credentials()
+
+def delete_credentials(credentials):
+    '''
+    A function that deletes credentials
+    '''
+
+    credentials.delete_credentials()
+
+def find_credentials(account):
+    '''
+    A function that finds credentials by account and returns them
+    '''
+
+    return Credentials.find_by_account(account)
+
+def check_existing_credentials(account):
+    '''
+    A function that checks if credentials exists with that account and returns a boolean
+    '''
+
+    return Credentials.credentials_exist(account)
+
+def display_credentials():
+    '''
+    A function that returns saved credentials
+    '''
+
+    return Credentials.display_credentials()
